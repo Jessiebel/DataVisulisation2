8b1f9fb68fa762aa785e215f481952527937bdb2
Jessiebel <jessicabellewhite@live.com>
2024-10-15 22:42:53 +1100

    fix index location

--- DataVisualisation2/src/js/chart.js
+++ DataVisualisation2/src/js/chart.js
@@ -1,6 +1,6 @@
-var vg_1 = "./json/ArrivalsMap.vg.json";
-var vg_2 = "./json/ArrivalsState.vg.json";
-var vg_3 = "./json/ArrivalVisas.vg.json";
+var vg_1 = "./src/json/ArrivalsMap.vg.json";
+var vg_2 = "./src/json/ArrivalsState.vg.json";
+var vg_3 = "./src/json/ArrivalVisas.vg.json";
 
 vegaEmbed("#Arrivals_Map_by_Home_Country", vg_1).catch(console.error);
 vegaEmbed("#Arrivals_Stacked_Area_by_State", vg_2).catch(console.error);
