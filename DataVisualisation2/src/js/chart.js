5ba400de06b5070b97055c0eecf5b0b0e48433a1
Jessiebel <jessicabellewhite@live.com>
2024-10-15 14:00:29 +1100

    finish arrivals by state

--- DataVisualisation2/src/js/chart.js
+++ DataVisualisation2/src/js/chart.js
@@ -1,4 +1,4 @@
 var vg_1 = "./json/ArrivalsMap.vg.json";
-var vg_2 = "./json/Visas.vg.json";
+var vg_2 = "./json/ArrivalsState.vg.json";
 vegaEmbed("#chart", vg_1).catch(console.error);
 vegaEmbed("#chart2", vg_2).catch(console.error);
