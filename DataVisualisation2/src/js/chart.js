ea204c5821c5cce5cb9744896bce53c508e91268
Jessiebel <jessicabellewhite@live.com>
2024-10-15 15:07:49 +1100

    upload visa data

--- DataVisualisation2/src/js/chart.js
+++ DataVisualisation2/src/js/chart.js
@@ -1,4 +1,7 @@
 var vg_1 = "./json/ArrivalsMap.vg.json";
 var vg_2 = "./json/ArrivalsState.vg.json";
+var vg_3 = "./json/ArrivalVisas.vg.json";
+
 vegaEmbed("#chart", vg_1).catch(console.error);
 vegaEmbed("#chart2", vg_2).catch(console.error);
+vegaEmbed("#chart3", vg_3).catch(console.error);
