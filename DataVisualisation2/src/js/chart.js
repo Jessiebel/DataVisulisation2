0f9bfdc9d5f54b6d9a04a1d0bcb939f44928169e
Jessiebel <jessicabellewhite@live.com>
2024-10-15 21:47:03 +1100

    formatting updates

--- DataVisualisation2/src/js/chart.js
+++ DataVisualisation2/src/js/chart.js
@@ -2,6 +2,6 @@ var vg_1 = "./json/ArrivalsMap.vg.json";
 var vg_2 = "./json/ArrivalsState.vg.json";
 var vg_3 = "./json/ArrivalVisas.vg.json";
 
-vegaEmbed("#chart", vg_1).catch(console.error);
-vegaEmbed("#chart2", vg_2).catch(console.error);
-vegaEmbed("#chart3", vg_3).catch(console.error);
+vegaEmbed("#Arrivals_Map_by_Home_Country", vg_1).catch(console.error);
+vegaEmbed("#Arrivals_Stacked_Area_by_State", vg_2).catch(console.error);
+vegaEmbed("#Arrivals_Stacked_Bar_by_Visa", vg_3).catch(console.error);
